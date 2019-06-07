e9f7bfd7ed1e46c149f008d951f4af7f18034ffa
johnson18404 <johnson18404@gmail.com>
2019-06-08 02:47:41 +0800

    6/7 update conn format

--- final_project/app/src/androidTest/java/com/example/final_project/ExampleInstrumentedTest.java
+++ final_project/app/src/androidTest/java/com/example/final_project/ExampleInstrumentedTest.java
@@ -28,21 +28,42 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    Context appContext;
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.final_project", appContext.getPackageName());
 
+        String res = UploadImage("2.jpg");
+
+        Log.d("res", res);
+
+        if (res.equals("fail")) {
+            Log.d("test", "fail");
+
+            return;
+        }
+        else {
+            Log.d("test", "keep going...");
+        }
+
+
+
+    }
+
+
+    private String UploadImage(String attachmentFileName) throws Exception {
         String TAG = "uploadTest";
 
-        Log.d(TAG, "world.");
-        Log.d(TAG, appContext.getFilesDir().getAbsolutePath());
+       //  Log.d(TAG, "world.");
+       // Log.d(TAG, appContext.getFilesDir().getAbsolutePath());
 
 
         String attachmentName = "image";
-        String attachmentFileName = "3.jpg";
+        // String attachmentFileName = filename;
 
         String crlf = "\r\n";
         String twoHyphens = "--";
@@ -50,66 +71,67 @@ public class ExampleInstrumentedTest {
         File FileToUpload = new File(appContext.getFilesDir(), attachmentFileName);
 
         //try {
-            // setup
-            HttpURLConnection conn = (HttpURLConnection) new URL("http://imt2019.iamss.cc:5000/lasso").openConnection();
-            conn.setUseCaches(false);
-            conn.setDoOutput(true);
-            conn.setRequestMethod("POST");
-            // conn.setRequestProperty("Connection", "Keep-Alive");
-            conn.setRequestProperty("Cache-Control", "no-cache");
-            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+        // setup
+        HttpURLConnection conn = (HttpURLConnection) new URL("http://imt2019.iamss.cc:5000/lasso").openConnection();
+        conn.setUseCaches(false);
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        // conn.setRequestProperty("Connection", "Keep-Alive");
+        conn.setRequestProperty("Cache-Control", "no-cache");
+        conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
 
-            // content wrapper
-            DataOutputStream request = new DataOutputStream(conn.getOutputStream());
-            request.writeBytes(twoHyphens + boundary + crlf);
-            request.writeBytes("Content-Disposition: form-data; name=\"" +
-                    attachmentName + "\";filename=\"" +
-                    attachmentFileName + "\"" + crlf);
-            request.writeBytes(crlf);
+        // content wrapper
+        DataOutputStream request = new DataOutputStream(conn.getOutputStream());
+        request.writeBytes(twoHyphens + boundary + crlf);
+        request.writeBytes("Content-Disposition: form-data; name=\"" +
+                attachmentName + "\";filename=\"" +
+                attachmentFileName + "\"" + crlf);
+        request.writeBytes(crlf);
 
-            // write data
-            FileInputStream inputStreamToLogFile = new FileInputStream(FileToUpload);
-            int bytesRead;
-            byte[] dataBuffer = new byte[1024];
-            while((bytesRead = inputStreamToLogFile.read(dataBuffer)) != -1) {
-                request.write(dataBuffer, 0, bytesRead);
-            }
-            request.flush();
+        // write data
+        FileInputStream inputStreamToLogFile = new FileInputStream(FileToUpload);
+        int bytesRead;
+        byte[] dataBuffer = new byte[1024];
+        while((bytesRead = inputStreamToLogFile.read(dataBuffer)) != -1) {
+            request.write(dataBuffer, 0, bytesRead);
+        }
+        request.flush();
 
-            // end content wrapper
-            request.writeBytes(crlf);
-            request.writeBytes(twoHyphens + boundary + twoHyphens + crlf);
+        // end content wrapper
+        request.writeBytes(crlf);
+        request.writeBytes(twoHyphens + boundary + twoHyphens + crlf);
 
-            // flush output buffer
-            request.flush();
-            request.close();
+        // flush output buffer
+        request.flush();
+        request.close();
 
 
 
-            // get response
-            InputStream responseStream = new BufferedInputStream(conn.getInputStream());
+        // get response
+        InputStream responseStream = new BufferedInputStream(conn.getInputStream());
 
-            BufferedReader responseStreamReader = new BufferedReader(new InputStreamReader(responseStream));
+        BufferedReader responseStreamReader = new BufferedReader(new InputStreamReader(responseStream));
 
-            String line = "";
-            StringBuilder stringBuilder = new StringBuilder();
+        String line = "";
+        StringBuilder stringBuilder = new StringBuilder();
 
-            while ((line = responseStreamReader.readLine()) != null) {
-                stringBuilder.append(line).append("\n");
-            }
-            responseStreamReader.close();
+        while ((line = responseStreamReader.readLine()) != null) {
+            stringBuilder.append(line).append("\n");
+        }
+        responseStreamReader.close();
 
-            String response = stringBuilder.toString();
-            Log.d(TAG, response);
+        String response = stringBuilder.toString();
+        // Log.d(TAG, response);
 
-            // close response stream & close connection
-            responseStream.close();
-            conn.disconnect();
+        // close response stream & close connection
+        responseStream.close();
+        conn.disconnect();
 
 
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
 
+        return response;
     }
 }
